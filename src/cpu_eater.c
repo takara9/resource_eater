3094a962e64e4b7303daf49c14d369e739e19107
Maho Takara <takara9@gmail.com>
2018-08-07 13:09:43 +0000

    update

--- src/cpu_eater.c
+++ src/cpu_eater.c
@@ -1,40 +1,12 @@
-
-#include <signal.h>
-#include <unistd.h>
 #include <stdio.h>
-#include <setjmp.h>
 #include <math.h>
-
-int f;
-
-void signal_handler (int x)
-{
-  f = 1;
-  return;
-}
-
 
 int main (int argc, char ** argv)
 {
-  int busy = atoi(argv[1]);
-  int wait = 1000 - busy;
-  if (wait < 0) wait = 0;
-  printf("busy = %d\n",busy);
-  printf("wait = %d\n",wait);
-  struct sigaction act;
-  act.sa_handler = signal_handler;
-  sigaction (SIGALRM, &act, 0);
   while(1) {
-    ualarm (busy, 0);
-    for(f = 0;f==1;) {
-      int n = 0;
-      for(int j = 0;j < 10000;j++) {
-	n++;
-      }
-    }
-    if (wait > 0) {
-      ualarm (wait, 0);
-      sleep(1);
+    int i;
+    for(i = 1; i < 1000; i++) {
+      float x = sqrtf((float)i);
     }
   }
   return 0;
